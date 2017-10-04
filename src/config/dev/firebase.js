74277aa48a7228c29e8b000026712023835abff6
RyanQuey <rlquey2@Gmail.com>
2017-10-04 00:54:44 -0700

    remove the specific firebase configuration

--- src/config/dev/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyD6xIDzLVarCb68_NRxiSlJxD9mOB6_sAM",
-  authDomain: "sequel-tracker.firebaseapp.com",
-  databaseURL: "https://sequel-tracker.firebaseio.com",
-  projectId: "sequel-tracker",
-  storageBucket: "sequel-tracker.appspot.com",
-  messagingSenderId: "781744309936"
-}
