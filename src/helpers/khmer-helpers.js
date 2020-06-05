2fb0ff8f9be7a5c7fc4287806e699adbd4ce63a4
Ryan Quey <quey.ryan@gmail.com>
2020-06-05 06:28:10 +0000

    and another

--- src/helpers/khmer-helpers.js
+++ src/helpers/khmer-helpers.js
@@ -65,6 +65,7 @@ window.MULTI_WORD_KHMER_PUNCTUATION = {
     // Google has read it this way before, so being flexible
     ["បើក", "សញ្ញា", "អាច", "ប្រកាស"],
     ["សញ្ញា", "បើក", "អាច", "ប្រកាស"],
+    ["សញ្ញា", "បើក", "ៗ", "ប្រកាស"],
     ["បើក", "សញ្ញា", "អញ្ញ"],
     // Google has read it this way before, so being flexible
     ["បើក", "សញ្ញា", "ៗ"],
@@ -77,6 +78,7 @@ window.MULTI_WORD_KHMER_PUNCTUATION = {
     ["បិទ", "សញ្ញា", "អអាច", "ប្រកាស"],
     ["បិទ", "សញ្ញា", "អញ្ញ", "ប្រកាស"],
     ["សញ្ញា", "បិទ", "អញ្ញ", "ប្រកាស"],
+    ["សញ្ញា", "បិទ", "ៗ", "ប្រកាស"],
     ["បិទ", "សញ្ញា", "អញ្ញ"],
     // Google has read it this way before, so being flexible
     ["បិទ", "សញ្ញា", "ៗ"],
