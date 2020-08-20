aac643dff495e3cc3bb327f2ec59d2f804b58ed2
Ryan Quey <quey.ryan@gmail.com>
2020-08-20 13:08:17 -0700

    tried and failed to use the svg

--- src/shared/components/elements/Logo/index.js
+++ src/shared/components/elements/Logo/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 //import logo from 'images/khmervoiceapp.png'
 import logo from 'images/sbbic-logo-text-high-km.png'
+import svglogo from 'images/khmervoice-logo-clear-bkgd.svg'
+import classes from './style.scss'
 
+// not getting this working right
+const both = false 
 const Logo = () => (
-  <img id="navbar-brand" alt="logo" src={logo} />
+  both ? (
+    <div className="container">
+      <img id="navbar-brand-svg" className={classes.darkBg} alt="logo2" src={svglogo} />
+      <img id="navbar-brand" alt="logo" src={logo} />
+    </div>
+  ) : (
+    <img id="navbar-brand" alt="logo" src={logo} />
+  )
 )
 
 export default Logo
